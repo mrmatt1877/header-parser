f5d7a26eaa5a41b4e525e85f2bb2239970f97239
Matt <mrmatt1877@gmail.com>
2016-06-02 23:12:01 +0000

    testing

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
-var app = server();
+var app = express();
 var useragent = require('useragent');
 
- app.get("/api/whoami/", function(req, res) {
+ app.get("/", function(req, res) {
    var ip = req.headers['x-forwarded-for'];
    var language = req.headers['accept-language'].split(",")[0];
    var software = useragent.parse(req.headers['user-agent']);
