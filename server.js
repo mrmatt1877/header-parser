db01ebe9d914c3c38f2df6eb4ad8c9fce0606cc4
Matt <mrmatt1877@gmail.com>
2016-06-02 22:25:23 +0000

    adding index.html

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
 
+ app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+ });
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function(){
-    console.log("listenting on" + port);
+    console.log("listenting on " + port);
 })
